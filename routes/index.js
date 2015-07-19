ac695f1312e7066782d41316ad900c56dfac84ad
Raül Pérez <raul.perez@r3labs.io>
2015-07-20 00:49:02 +0200

    Show connections

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,31 @@ var request = require('request');
 
 var router = express.Router();
 
+var base = 'http://localhost:8222';
+
 router.get('/', function(req, res, next) {
+    'use strict';
+
     var context = {
         title: 'NATS.io monitor'
     };
-    request('http://localhost:8222/varz', function(error, response, body) {
+
+    // varz
+    request(base + '/varz', function(error, response, body) {
         if (!error && response.statusCode == 200) {
             context.varz = JSON.parse(body);
-            res.render('index', context);
+
+            // connz
+            request(base + '/connz', function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    context.connz = JSON.parse(body);
+
+                    res.render('index', context);
+                } else {
+                    next(error);
+                }
+            });
+
         } else {
             next(error);
         }
